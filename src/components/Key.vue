45c9661789e8f3aeae3c7ee3b22a3d8de259fe06
varun <varun@validus.sg>
2022-04-02 21:48:59 +0530

    Create key component

---
+++ src/components/Key.vue
@@ -0,0 +1,33 @@
+<template>
+  <button
+    @click="onClickButton"
+    class="text-black cursor-pointer rounded-borders"
+  >
+    {{ name }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Key",
+  props: ["name", "type"],
+  methods: {
+    onClickButton() {
+      this.$emit("clicked", [this.type, this.name]);
+    }
+  }
+};
+</script>
+
+<style scoped>
+button {
+  transition-duration: 0.2s;
+  border: 1px solid white;
+  background: white;
+  min-width: 30px;
+  margin: 2px;
+}
+button:hover {
+  border: 1px solid black;
+}
+</style>
