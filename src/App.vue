4cd1464894c7d4b0a51d8c0b7d75c63ffae1f67d
varun <varun@validus.sg>
2022-04-02 21:48:45 +0530

    Create base for project

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h3>{{ applicationName }}</h3>
+    <Keyboard />
+  </div>
+</template>
+
+<script>
+import Keyboard from "./components/Keyboard.vue";
+const applicationName = "Virtual Keyboard";
+export default {
+  name: "App",
+  components: {
+    Keyboard
+  },
+  data() {
+    return {
+      applicationName
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/utils.js
@@ -0,0 +1,30 @@
+export const alphabetKeys = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+export const numericKeys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+export const functionKeys = ["space", "delete", "enter", "shift", "caps"];
